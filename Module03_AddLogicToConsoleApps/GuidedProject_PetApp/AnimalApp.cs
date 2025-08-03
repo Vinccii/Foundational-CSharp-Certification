9c72795078def6a7aec5b3aaf3f9db114ac755d3
Vincent <vinciheinzelmann@gmail.com>
2025-08-03 12:26:13 +0200

    🐾 Feature: Display all pet characteristics using nested for loops (Exercise 2)

--- Module03_AddLogicToConsoleApps/GuidedProject_PetApp/AnimalApp.cs
+++ Module03_AddLogicToConsoleApps/GuidedProject_PetApp/AnimalApp.cs
@@ -125,7 +125,17 @@ namespace Module03_AddLogicToConsoleApps.GuidedProject_PetApp
                 {
                     case "1":
                         // List all of our current pet information
-                        Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+                        for (int i = 0; i < maxPets; i++)
+                        {
+                            if (ourAnimals[i, 0] != "ID #: ")
+                            {
+                                Console.WriteLine();
+                                for (int j = 0; j < 6; j++)
+                                {
+                                    Console.WriteLine(ourAnimals[i, j]);
+                                }
+                            }
+                        }
                         Console.WriteLine("Press the Enter key to continue.");
                         readResult = Console.ReadLine();
                         break;
