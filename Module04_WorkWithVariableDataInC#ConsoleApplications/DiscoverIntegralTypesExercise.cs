772939baab278aef228aa183a2ab0b89cd825ae1
Vincent <vinciheinzelmann@gmail.com>
2025-08-31 19:43:59 +0200

    Add DiscoverIntegralTypesExercise (Module04)

---
+++ Module04_WorkWithVariableDataInC#ConsoleApplications/DiscoverIntegralTypesExercise.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Module04_WorkWithVariableDataInCSharpConsoleApplications.Exercises
+{
+    public static class DiscoverIntegralTypesExercise
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Exercise – Discover Integral Types");
+
+            Console.WriteLine("Signed integral types:");
+
+            Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+            Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+            Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+            Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+            Console.WriteLine("");
+            Console.WriteLine("Unsigned integral types:");
+
+            Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+            Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+            Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+            Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+        }
+    }
+}
--- Module04_WorkWithVariableDataInC#ConsoleApplications/Program.cs
+++ Module04_WorkWithVariableDataInC#ConsoleApplications/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
-using Module04_WorkWithVariableDataInCSharpConsoleApplications;
+﻿using Module04_WorkWithVariableDataInCSharpConsoleApplications.Exercises;
 
 Console.WriteLine("Module 04 – Work With Variable Data in C#");
+DiscoverIntegralTypesExercise.Run();
