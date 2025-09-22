9a1363708e40d6b1b373b31e28a8c0ac479b0006
Vincent <vinciheinzelmann@gmail.com>
2025-09-22 18:59:05 +0200

    feat: implement logic to combine string array values as message and total

--- Module04_WorkWithVariableDataInC#ConsoleApplications/CombineStringArrayValuesAsStringsAndIntergersExercise.cs
+++ Module04_WorkWithVariableDataInC#ConsoleApplications/CombineStringArrayValuesAsStringsAndIntergersExercise.cs
@@ -6,7 +6,26 @@ namespace Module04_WorkWithVariableDataInC_ConsoleApplications
     {
         public static void Run()
         {
+            string[] values = { "12.3", "45", "ABC", "11", "DEF" };
 
+            double total = 0;
+            string message = "";
+
+            foreach (string value in values)
+            {
+                if (double.TryParse(value.Replace(".", ","), out double number))
+                {
+                    total += number;
+
+                }
+                else
+                {
+                    message += value;
+                }
+            }
+
+            Console.WriteLine($"message: {message}");
+            Console.WriteLine($"Todal: {total}");
         }
     }
 }
